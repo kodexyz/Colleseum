b89f4b17f77de64b45e4dbfc878de82602290292
AwesomeHaircuT <jesusbaslbastro@gmail.com>
2018-07-26 18:28:14 -0700

    Added confession, refactor a bit and fixed the shuhs functionality

--- main.py
+++ main.py
@@ -23,39 +23,40 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
-    content = message.content.lower()  # This makes the bot ignore case
     if message.author == client.user:
         return
+    content = message.content.lower()  # This makes the bot ignore case
+    channel = message.channel  # this makes sense to me
     if "yler" in message.author.name: #  This makes the bot ignore Tyler. or anyone with yler in their name
         msg = "I'm not gonna talk to you"  # Freakin tyler
-        client.send_message(message.channel, msg)
+        client.send_message(channel, msg)
         return
 
     if content.startswith("!hello"):
         msg = "Hello {0.author.mention}".format(message) # I have no idea what the heck is up with those curly brackets
-        await client.send_message(message.channel, msg)  # This bit is just part of the tutorial I followed
+        await client.send_message(channel, msg)  # This bit is just part of the tutorial I followed
                                                          # But it literally didn't explain anything
     if content.startswith(activator + "fortune"):
         print(message.author.id)
 
         msg = r("fortune")  # The r function is just a wrapper to the run function in subprocess
                                 # Basically it runs a command line program and spits back the result
-        await client.send_message(message.channel, msg)
+        await client.send_message(channel, msg)
     # the rest is sort of self explanatory
     if "bong" in content:
         msg = "Drugs are bad mmmmkay"
-        await client.send_message(message.channel, msg)
+        await client.send_message(channel, msg)
     if "despacito" in content:
         msg = "let's not despacITo"
-        await client.send_message(message.channel, msg)
+        await client.send_message(channel, msg)
 
     if message.content.startswith(activator + "Skeet"):
         msg = "i am a robot I have not emotions"
-        await client.send_message(message.channel, msg)
+        await client.send_message(channel, msg)
 
     if message.content.startswith(activator + "givemethedrugs"):
         msg = "no"
-        await client.send_message(message.channel, msg)
+        await client.send_message(channel, msg)
 
     if content.startswith("banner"): #This bit of code is proned to bugs
         print(message.content)
@@ -73,27 +74,33 @@ async def on_message(message):
         print(msg)
         print(len(msg))
         if len(msg) < 9:
-            await client.send_message(message.channel, "Empty. Like my soul")
+            await client.send_message(channel, "Empty. Like my soul")
         else:
-            await client.send_message(message.channel, msg)
+            await client.send_message(channel, msg)
 
     if "identity" in message.content:
         msg = r("rig")
-        await client.send_message(message.channel, msg)
+        await client.send_message(channel, msg)
 
     if message.content.startswith("!home"):
         msg = "https://github.com/awesomehaircut/Colleseum"
-        await client.send_message(message.channel, msg)
+        await client.send_message(channel, msg)
 
     if "shush" in content:
         msg = "I'll be quiet for for a little bit"
-        await client.send_message(message.channel, msg)
+        await client.send_message(channel, msg)
         sleep(360)
-        await client.send_message(message, "I'm not grouned anymore hehe")
+        await client.send_message(channel, "Ok, i'm back")
 
     if "colleseum" in content:
         msg = "I love when people talk about me"
-        await client.send_message(message.channel, msg)
+        await client.send_message(channel, msg)
+    if content.startswith("confession"):
+        if str(channel).startswith("confess"):
+            await client.send_message(channel, message.content)
+            await client.delete_message(message)
+        else:
+            await client.send_message(channel, "Sorry that only works in #confessions")
 
 
 @client.event
